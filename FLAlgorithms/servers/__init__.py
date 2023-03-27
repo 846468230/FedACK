c4527676d0bebf9d228e912bd188a41b78f24085
Yingguang Yang <32445858+846468230@users.noreply.github.com>
2023-03-27 16:53:27 +0800

    Add files via upload

--- FLAlgorithms/servers/__init__.py
+++ FLAlgorithms/servers/__init__.py
@@ -1,2 +1,6 @@
 from .serverbase import Server
 from .serverpFedACK import FedACK
+from .serverage import FedAvg
+from .serverFedProx import FedProx
+from .serverFedDistill import FedDistill
+from .serverFedEnsemble import FedEnsemble
